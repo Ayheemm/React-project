2f2cd2195d0bbd80c33912c02c0a01af42b3768b
Ayheemm <ayhem.hamdi8@gmail.com>
2025-11-15 16:15:42 +0100

    doing a movie card

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import MovieCard from "./components/MovieCard" ;
+
+function App() {
+  
+
+  return (
+    <>
+     <MovieCard movie={{title: "Mr Robot", release_date: "2015-07-16", }} />
+    </>
+  )
+}
+
+
+export default App;
